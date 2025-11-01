46e337e37a71a916494aeafabd644461570ed6f9
Amal s kumar <amalskumarofficialz@gmail.com>
2025-11-01 18:00:18 +0530

    working lexer for our lang

--- src/lexer.c
+++ src/lexer.c
@@ -84,7 +84,7 @@ token_T* lexer_collect_id(lexer_T* lexer){
     char* value = calloc(1, sizeof(char));
     value[0] = '\0';
     while(isalnum(lexer->c)){
-        printf("Char: %c\n", lexer->c);
+        // printf("Char: %c\n", lexer->c);
         char* s = lexer_get_current_char_as_string(lexer);
         value = realloc(value, (strlen(value) + strlen(s) + 1)*sizeof(char));
         strcat(value, s);
